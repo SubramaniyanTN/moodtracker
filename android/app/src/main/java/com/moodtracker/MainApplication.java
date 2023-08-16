7b73922dfc5abdb08a5455c766d4a5f64133875c
subramaniyan <subramaniyanm@xbi4.com>
2023-08-16 14:22:15 +0530

    react-native-reanimated package added

--- android/app/src/main/java/com/moodtracker/MainApplication.java
+++ android/app/src/main/java/com/moodtracker/MainApplication.java
@@ -9,6 +9,8 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import com.facebook.react.bridge.JSIModulePackage; 
+// import com.swmansion.reanimated.ReanimatedJSIModulePackage; 
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -32,6 +34,10 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+        //  @Override
+        //  protected JSIModulePackage getJSIModulePackage() {
+        //   return new ReanimatedJSIModulePackage(); 
+        //   }
 
         @Override
         protected boolean isNewArchEnabled() {
